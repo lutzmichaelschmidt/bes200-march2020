fbda45c6d437568da1e64c417ed915ee04329fc4
Jeffry Gonzalez <jeff@hypertheory.com>
2020-02-24 20:58:46 +0000

    Done with Monday

---
+++ LibraryApi/Controllers/StatusController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class StatusController : Controller
+    {
+        [HttpGet("status")]
+        public ActionResult<StatusResponse> GetTheStatus()
+        {
+            var response = new StatusResponse
+            {
+                Status = "Looks good up here, captain!",
+                CreatedAt = DateTime.Now
+            };
+            return Ok(response);
+        }
+    }
+
+    public class StatusResponse
+    {
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
