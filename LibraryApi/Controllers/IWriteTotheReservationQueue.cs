91c948f15f017521a79e15d5c0a61211f1bb0cec
lutzmichaelschmidt <61434839+lutzmichaelschmidt@users.noreply.github.com>
2020-04-02 19:55:22 +0000

    End of Day 3

---
+++ LibraryApi/Controllers/IWriteTotheReservationQueue.cs
@@ -0,0 +1,10 @@
+﻿using LibraryApi.Domain;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public  interface IWriteTotheReservationQueue
+    {
+        Task Write(Reservation reservation);
+    }
+}
--- LibraryApi/Startup.cs
+++ LibraryApi/Startup.cs
@@ -17,6 +17,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RabbitMqUtils;
 
 namespace LibraryApi
 {
@@ -32,6 +33,8 @@ namespace LibraryApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRabbit(Configuration);
+            services.AddTransient<IWriteTotheReservationQueue, RabbitMqReservationProcessor>();
             services.AddTransient<ILookupOnCallDevelopers, DeveloperCallLookup>();
             services.AddTransient<IGenerateEmployeeIds, EmployeeIdGenerator>();
             services.AddControllers().AddJsonOptions(options =>
